2be423e3d7ed77d9ae5a6aff140f225a31fb5d3f
Jimmy Vlekke <jimmyvlekke@gmail.com>
2021-04-01 11:14:40 +0200

    Fix indentation based on PR suggestion

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -3,41 +3,41 @@ const removeActiveClasses = function (ulElement) {
     Array.prototype.forEach.call(lis, function(li) {
         li.classList.remove('active');
     });
-  }
+}
 
-  const getChildPosition = function (element) {
-        var parent = element.parentNode;
-        var i = 0;
-        for (var i = 0; i < parent.children.length; i++) {
-            if (parent.children[i] === element) {
-                return i;
-            }
+const getChildPosition = function (element) {
+    var parent = element.parentNode;
+    var i = 0;
+    for (var i = 0; i < parent.children.length; i++) {
+        if (parent.children[i] === element) {
+            return i;
         }
-
-        throw new Error('No parent found');
     }
+
+    throw new Error('No parent found');
+}
 
 window.addEventListener('load', function () {
     const tabLinks = document.querySelectorAll('ul.tab li a');
 
     Array.prototype.forEach.call(tabLinks, function(link) {
-      link.addEventListener('click', function (event) {
-        event.preventDefault();
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
 
-        liTab = link.parentNode;
-        ulTab = liTab.parentNode;
-        position = getChildPosition(liTab);
-        if (liTab.className.includes('active')) {
-          return;
-        }
+            liTab = link.parentNode;
+            ulTab = liTab.parentNode;
+            position = getChildPosition(liTab);
+            if (liTab.className.includes('active')) {
+                return;
+            }
 
-        removeActiveClasses(ulTab);
-        tabContentId = ulTab.getAttribute('data-tab');
-        tabContentElement = document.getElementById(tabContentId);
-        removeActiveClasses(tabContentElement);
+            removeActiveClasses(ulTab);
+            tabContentId = ulTab.getAttribute('data-tab');
+            tabContentElement = document.getElementById(tabContentId);
+            removeActiveClasses(tabContentElement);
 
-        tabContentElement.querySelectorAll('li')[position].classList.add('active');
-        liTab.classList.add('active');
-      }, false);
+            tabContentElement.querySelectorAll('li')[position].classList.add('active');
+            liTab.classList.add('active');
+        }, false);
     });
 });
